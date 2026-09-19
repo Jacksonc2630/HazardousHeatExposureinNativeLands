414266f8bbc16a1173069731de54f1382d4ff193
Jackson Chen <Jacksonc2630@gmail.com>
2026-09-19 18:38:17 -0400

    Delete data directory

--- data/file_locations/file_locations.R
+++
@@ -1,4 +0,0 @@
-# list of important file locations to avoid having to repeat every time loading files
-raw_wbgt_native_data = here::here('data/wbgt_raw/native/')
-raw_wbgt_state_data = here::here('data/wbgt_raw/state/')
-population_data = here::here('data/population/')
